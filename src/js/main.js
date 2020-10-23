b8f0c2ca0c1fdc9656dfead3620b987f5992302e
Kazuma Hitomi <hitomi@bankto.co.jp>
2020-10-24 02:30:32 +0900

    template ブラッシュアップ

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,12 @@
 import 'core-js/stable'
 import 'regenerator-runtime/runtime'
 import '../scss/main.scss'
+import Top from './top'
+;(() => {
+  const top = new Top()
+  const canvas = document.getElementById('canvas')
+
+  top.init(canvas)
+
+  window.addEventListener('resize', () => top.resize(), { passive: true })
+})()
--- src/js/top.js
+++ src/js/top.js
@@ -1,49 +1,83 @@
 import '../scss/page/top.scss'
 import {
   WebGLRenderer,
-  OrthographicCamera,
+  // OrthographicCamera,
+  PerspectiveCamera,
   Scene,
-  PlaneGeometry,
-  ShaderMaterial,
+  // PlaneGeometry,
+  CubeGeometry,
+  // ShaderMaterial,
+  MeshNormalMaterial,
   Mesh,
 } from 'three'
 
-import vertexShader from '../shader/main.vert'
-import fragmentShader from '../shader/main.frag'
+// import vertexShader from '../shader/main.vert'
+// import fragmentShader from '../shader/main.frag'
 
-class Top {
+export default class Top {
   constructor() {
-    this.w = window.innerWidth
-    this.h = window.innerHeight
+    this.size = null
+    this.renderer = null
+    this.camera = null
+    this.scene = null
+    this.mesh = null
+  }
+
+  windowSize() {
+    this.size = {
+      windowWidth: window.innerWidth,
+      windowHeight: window.innerHeight,
+    }
+  }
+
+  init($canvas) {
+    this.windowSize()
 
     this.renderer = new WebGLRenderer()
-    this.renderer.setSize(this.w, this.h)
-    this.renderer.setPixelRatio(window.devicePixelRatio)
+    this.renderer.setSize(this.size.windowWidth, this.size.windowHeight)
+    this.renderer.setClearColor(0x000000)
+    this.renderer.setPixelRatio(window.devicePixelRatio || 1)
+    $canvas.appendChild(this.renderer.domElement)
 
-    const canvas = document.getElementById('canvas')
-    canvas.appendChild(this.renderer.domElement)
+    // this.camera = new OrthographicCamera(-1, 1, 1, -1, 0, 1)
+    this.camera = new PerspectiveCamera()
+    this.camera.position.set(0, 10, 150)
 
-    this.camera = new OrthographicCamera(-1, 1, 1, -1, 0, 1)
-
-    this.scene = new Scene()
-
-    const geo = new PlaneGeometry(2, 2, 10, 10)
-    const mat = new ShaderMaterial({
-      vertexShader,
-      fragmentShader,
-      wireframe: true,
-    })
-    this.mesh = new Mesh(geo, mat)
-
-    this.scene.add(this.mesh)
-
+    this.shape()
     this.render()
   }
 
+  shape() {
+    this.scene = new Scene()
+    // const geo = new PlaneGeometry(2, 2, 10, 10)
+    // const mat = new ShaderMaterial({
+    //   vertexShader,
+    //   fragmentShader,
+    //   wireframe: true,
+    // })
+    const geo = new CubeGeometry(50, 50, 50)
+    const mat = new MeshNormalMaterial()
+    this.mesh = new Mesh(geo, mat)
+    this.mesh.position.set(0, 5, 1)
+    this.scene.add(this.mesh)
+  }
+
+  animation() {
+    this.mesh.rotation.x += 0.01
+    this.mesh.rotation.y += 0.01
+    this.mesh.rotation.z += 0.01
+  }
+
   render() {
-    requestAnimationFrame(() => this.render)
+    this.animation()
+    requestAnimationFrame(() => this.render())
     this.renderer.render(this.scene, this.camera)
   }
-}
 
-new Top()
+  resize() {
+    this.windowSize()
+    this.camera.aspect = this.size.windowWidth / this.size.windowHeight
+    this.camera.updateProjectionMatrix()
+    this.renderer.setSize(this.size.windowWidth, this.size.windowHeight)
+  }
+}
